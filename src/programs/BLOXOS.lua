0c075a82b41584da2a61696cbbce7e22fee9e2a5
Beriff <dofite.yes@gmail.com>
2020-10-12 21:10:12 +0700

    Update BLOXOS.lua

--- src/programs/BLOXOS.lua
+++ src/programs/BLOXOS.lua
@@ -1,5 +1,5 @@
 --TODO OS
-BIOSAPIS["sendMonitorSignal"]("set", "BLOXOS will be installed now. Proceed?")
+BIOSAPIS["sendMonitorSignal"]("set", "BLOXOS will be installed now. Y/N")
 if BIOSAPIS["detectUserInput"]() == "y" then
     BIOSAPIS["sendMonitorSignal"]("add", "okie dokie!")
 end
