ca21bc8180d79848cfa08e3df7ccc68aab9abd95
Beriff <dofite.yes@gmail.com>
2020-10-13 20:15:06 +0700

    Added OS loading to hard drive

--- src/programs/BLOXOS/bootloader.lua
+++ src/programs/BLOXOS/bootloader.lua
@@ -3,5 +3,6 @@
 BIOSAPIS["sendMonitorSignal"]("set", "BLOXOS will be installed now. Y/N \n")
 if BIOSAPIS["detectUserInput"]() == "y" then
     BIOSAPIS["sendMonitorSignal"]("add", "OK. Installing BLOXOS now.")
-    
+    HDDmodule.exactAddContent(GET('https://raw.githubusercontent.com/Beriff/BLOXPC/main/src/programs/BLOXOS/BLOXOS.lua'), 0)
+    BIOSAPIS["sendMonitorSignal"]("set", "BLOXOS installed. \n Pull the disk and reboot the pc.")
 end
