d77abb46366752e8b2427fcfdaa9f971118de278
Beriff <dofite.yes@gmail.com>
2020-10-14 20:43:09 +0700

    Update and rename BSFS.lua to SMF.lua

---
+++ src/programs/BLOXOS/components/SMF.lua
@@ -0,0 +1,13 @@
+SMF = {}
+gAddr = 0
+function genFile(name, content, ext)
+	gAddr = gAddr + 1
+	file = {}
+	file[0] = name
+	file[1] = content
+	file[2] = ext
+	SMF[gAddr] = file
+	return file
+end
+
+genFile('test', 'hello, world!', 'text')
