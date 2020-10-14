dd31977114340bea3b0c955b0b5d77046238e8f3
Beriff <dofite.yes@gmail.com>
2020-10-14 11:01:42 +0700

    change GET request to raw.githubusercontent.com

--- src/programs/BLOXOS/BLOXOS.lua
+++ src/programs/BLOXOS/BLOXOS.lua
@@ -1,6 +1,6 @@
 --TODO OS
-os_setup, err = GET('https://raw.githack.com/Beriff/BLOXPC/main/src/programs/BLOXOS/components/os_setup.lua')
-print(err)
+os_setup, err = GET('https://raw.githubusercontent.com/Beriff/BLOXPC/main/src/programs/BLOXOS/components/os_setup.lua')
+--print(os_setup)
 setup = loadstring(os_setup)
 option = setup()
 print('option')
