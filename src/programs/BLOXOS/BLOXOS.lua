283470018f477dd07c0fed54afe11beb3bc89d1f
Beriff <dofite.yes@gmail.com>
2020-10-14 10:53:32 +0700

    debug

--- src/programs/BLOXOS/BLOXOS.lua
+++ src/programs/BLOXOS/BLOXOS.lua
@@ -1,6 +1,6 @@
 --TODO OS
-os_setup = GET('https://raw.githack.com/Beriff/BLOXPC/main/src/programs/BLOXOS/components/os_setup.lua')
-print(os_setup)
+os_setup, err = GET('https://raw.githack.com/Beriff/BLOXPC/main/src/programs/BLOXOS/components/os_setup.lua')
+print(err)
 setup = loadstring(os_setup)
 option = setup()
 print('option')
