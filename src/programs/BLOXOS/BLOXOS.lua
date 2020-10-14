c0720a22e73cfd7254c4077327704f5a2108f259
Beriff <dofite.yes@gmail.com>
2020-10-14 20:51:59 +0700

    Update BLOXOS.lua

--- src/programs/BLOXOS/BLOXOS.lua
+++ src/programs/BLOXOS/BLOXOS.lua
@@ -1,6 +1,10 @@
 --TODO OS
-os_setup = GET('https://raw.githubusercontent.com/Beriff/BLOXPC/main/src/programs/BLOXOS/components/os_setup.lua')
-setup, err = loadstring(os_setup)
-print(err)
-option = setup()
-print('option')
+if not HDDmodule.getContent(2) then
+ 	os_setup = GET('https://raw.githubusercontent.com/Beriff/BLOXPC/main/src/programs/BLOXOS/components/os_setup.lua')
+	setup, err = loadstring(os_setup)
+	--print(err)
+	option = setup()
+	BIOSAPIS["sendMonitorSignal"]("set", "BLOXOS is ready to use. Reboot PC to dive in!")
+	--print('option')
+else
+	BIOSAPIS["sendMonitorSignal"]("set", "Beriffsoft 2020 BLOXOS")
