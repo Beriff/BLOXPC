6675f2f25e8149bffc3c774d8ccd377ec6a0616e
Beriff <dofite.yes@gmail.com>
2020-10-14 17:00:12 +0700

    Update os_setup.lua

--- src/programs/BLOXOS/components/os_setup.lua
+++ src/programs/BLOXOS/components/os_setup.lua
@@ -20,7 +20,7 @@ function fsOptions()
 fsOptions()
 while true do
   clicked = BIOSAPIS["detectUserInput"]()
-  print('clicked')
+  print(clicked)
   if clicked == "upArr" or clicked == "downArr" then
     if currCursor then
     
